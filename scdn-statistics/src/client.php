2925deb5c9fe4c867e8b95c962710710865ef13d
xuhuitao <xuhuitao@onething.net>
2024-02-21 19:55:04 +0800

    composer init

---
+++ scdn-statistics/src/client.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xuhuitao
+ * Date: 2024/2/21
+ * Time: 19:43:50
+ */
+
+namespace galaxy\ScdnStatistics;
+
+class StatisticsClient {
+    public function test() {
+        return true;
+    }
+}
